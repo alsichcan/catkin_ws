9f501811fa0d10d49e49c5400cf997f7344596d5
alsichcan <jace.jisang.park@gmail.com>
2023-09-26 20:42:12 +0900

    topic

--- build/basics/CTestTestfile.cmake
+++ build/basics/CTestTestfile.cmake
@@ -1,8 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/park/dev/catkin_ws/src
-# Build directory: /home/park/dev/catkin_ws/build
+# Source directory: /home/park/dev/catkin_ws/src/basics
+# Build directory: /home/park/dev/catkin_ws/build/basics
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("basics")
--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/park/dev/catkin_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/park/dev/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/park/dev/catkin_ws/devel;/opt/ros/noetic' != '' else []
